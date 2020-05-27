7ce7313dd906adfa34e5b2259a3a9f47977347a4
safwankdb <kdbeatbox@gmail.com>
2020-05-27 17:01:25 +0530

    bump version to 0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="petyr",
-    version="0.0.4",
+    version="0.0.5",
     author="Mohd Safwam",
     author_email="kdbeatbox@gmail.com",
     description="2D Affine and Projective Transformations",
